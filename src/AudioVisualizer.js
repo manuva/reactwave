27dcd9ffd0233dc86b42c223ae6c9db6bbb38faa
manuva <fufanux@gmail.com>
2024-04-18 21:20:16 -0400

    Update AudioVisualizer.js

--- src/AudioVisualizer.js
+++ src/AudioVisualizer.js
@@ -69,7 +69,7 @@ const AudioVisualizer = () => {
 
       analyserNode.getByteFrequencyData(dataArray);
 
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+      ctx.fillStyle = 'transparent';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
       const barWidth = (canvas.width / bufferLength) * 2.5;
